f4fb07b25ab620bdf195cef29cc879fa84d4fbcf
jisu <wltn7498@naver.com>
2022-02-02 22:34:37 +0900

    220202_TIL_coding

--- 220201_BAEKJOON.py
+++ 220201_BAEKJOON.py
@@ -1,34 +1,36 @@
-# # 10818
+# 10818
 
-# n = int(input())
-# lst_num = list(map(int, input().split()))
-# max_num = lst_num[0]
-# min_num = lst_num[0]
-# for i in range(1, n):
-#     if max_num < lst_num[i]:
-#         max_num = lst_num[i]
-# for i in range(1, n):
-#     if min_num > lst_num[i]:
-#         min_num = lst_num[i]   
-# print(min_num,max_num)
+n = int(input())
+lst_num = list(map(int, input().split()))
+max_num = lst_num[0]
+min_num = lst_num[0]
+for i in range(1, n):
+    if max_num < lst_num[i]:
+        max_num = lst_num[i]
+for i in range(1, n):
+    if min_num > lst_num[i]:
+        min_num = lst_num[i]   
+print(min_num,max_num)
 
 
-# # 2562
-# max_num = int(input())
-# numbering = 1
-# for i in range(2,10):
-#     num_n = int(input())
-#     if max_num < num_n:
-#         max_num = num_n
-#         numbering = i
-# print(max_num, '/n', numbering)
+# 2562
+max_num = int(input())
+numbering = 1
+for i in range(2,10):
+    num_n = int(input())
+    if max_num < num_n:
+        max_num = num_n
+        numbering = i
+print(max_num, '/n', numbering)
 
 
 # 2577
 import sys
 a, b, c = list(int(sys.stdin.readline()) for _ in range(3)) 
-number = a*b*c
+number = str(a*b*c)
 result = [0]*10
-
 for i in number:
-    
+    result[int(i)] += 1
+for i in result:
+    print(i)
+
---
+++ 220202_BAEKJOON.py
@@ -0,0 +1,22 @@
+# 3052
+import sys
+lst = [sys.stdin.readline().strip() for i in range(10)]
+lst2 = []
+for num in lst:
+    lst2.append(int(num)%42)
+set1 = set(lst2)
+print(len(set1))
+
+
+# 1546
+num = int(input())
+scores = list(map(int, input().split()))
+x = 0
+max_score = max(scores)
+for i in scores:
+    x += (i/max_score)*100
+result = x/num
+print(result)
+
+
+# 8958
